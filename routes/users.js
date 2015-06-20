9eb007feafbdd292970fcd321ed5956dfb1fc11c
Jair Mac <jairrillo@gmail.com>
2015-06-20 18:00:40 -0300

    Added the CREATE and LIST functionalities

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,31 @@ var models		= require('../models');
 var Users 		= models.Users;
 
 /* GET users listing. */
-router.get('/create', function(req, res) {
-	
+router.get('/new', function(req, res) {
+	res.render('new');
+});
+
+router.post('/create', function(req, res) {
+	Users.create({name : req.body.name})
+	.then(function(user) {
+		console.log('Usuario criado com sucesso');
+		res.redirect('/');
+	})
+	.catch(function(err) {
+		console.log('Erro');
+		res.render('new', {errorMessage : 'Houve algum erro: ' + err});
+	});
+});
+
+router.get('/list', function(req, res) {
+	Users.findAll()
+	.then(function(users) {
+		console.log(JSON.stringify(users));
+		res.render('list', {result : users});
+	})
+	.catch(function(err) {
+		res.redirect('/');
+	})
 });
 
 module.exports = router;
